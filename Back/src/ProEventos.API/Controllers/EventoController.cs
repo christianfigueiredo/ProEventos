f2fe92589031281552eea4b08555f3878436c154
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2023-11-03 16:07:29 -0300

    migração codefirst

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -11,7 +12,9 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {          
-        public IEnumerable<Evento> _evento = new Evento[]
+        // retorno de eventos de forma estatica
+
+       /*  public IEnumerable<Evento> _evento = new Evento[]
             {
                 new Evento()
                 {
@@ -48,21 +51,23 @@ namespace ProEventos.API.Controllers
                     ImagemURL = "foto3.png"
                 }
 
-            };
-        public EventoController()
+            }; */
+        
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-           
+           _context = context;
         }
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;           
+            return _context.Eventos;           
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);           
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);           
         }
         [HttpPost]
         public string Post()
---
+++ Back/src/ProEventos.API/Data/Migrations/20231103182541_initial.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProEventos.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    EventoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Local = table.Column<string>(type: "TEXT", nullable: true),
+                    DataEvento = table.Column<string>(type: "TEXT", nullable: true),
+                    Tema = table.Column<string>(type: "TEXT", nullable: true),
+                    QtdPessoas = table.Column<int>(type: "INTEGER", nullable: false),
+                    ImagemURL = table.Column<string>(type: "TEXT", nullable: true),
+                    Telefone = table.Column<string>(type: "TEXT", nullable: true),
+                    Lote = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.EventoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eventos");
+        }
+    }
+}
