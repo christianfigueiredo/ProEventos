ac3b24b4330da4250d202e1909c41de36310e301
christian figueiredo <developer.figueiredo@gmail.com>
2026-07-16 13:48:07 -0300

    add http protocol

--- Back/src/ProEventos.API/obj/Debug/net6.0/ProEventos.API.AssemblyInfo.cs
+++ Back/src/ProEventos.API/obj/Debug/net6.0/ProEventos.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProEventos.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f687b97901b281e614adf8d005d145a18ffd4bf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc7aa71f8e361f9d88f60f3f822f947db3b72c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProEventos.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProEventos.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
