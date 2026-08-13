da2a28f8fa6a3a8812ab7d41c7583d632b7653f2
christian figueiredo <developer.figueiredo@gmail.com>
2026-08-13 13:20:18 -0300

    add architecture

--- Back/src/ProEventos.API/obj/Debug/net6.0/ProEventos.API.AssemblyInfo.cs
+++ Back/src/ProEventos.API/obj/Debug/net6.0/ProEventos.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProEventos.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5178950a47df85f2acb282381825225e43fe555")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7efd661f2bb61d259d8752126f48d3e0b01e4f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProEventos.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProEventos.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
